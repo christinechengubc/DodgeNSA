147efb5e0bf5c4159af3c87ba4f7374521b12a4a
pacoleee <pacohotin@hotmail.com>
2016-02-27 17:59:07 -0800

    Part 3

---
+++ DodgeNSA/core/src/com/mygdx/game/States/MenuState.java
@@ -0,0 +1,28 @@
+package com.mygdx.game.States;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+/**
+ * Created by pacolee on 16-02-27.
+ */
+public class MenuState extends State {
+
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+    }
+
+    @Override
+    public void handleInput() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+}
--- DodgeNSA/core/src/com/mygdx/game/States/State.java
+++ DodgeNSA/core/src/com/mygdx/game/States/State.java
@@ -1,4 +1,4 @@
-package com.mygdx.game.State;
+package com.mygdx.game.States;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -10,13 +10,13 @@ import com.badlogic.gdx.math.Vector3;
 public abstract class State {
     protected OrthographicCamera cam;
     protected Vector3 mouse;
-//    protected GameStateManager gsm;
-//
-//    protected State(GameStateManager gsm) {
-//        this.gsm = gsm;
-//        cam = new OrthographicCamera();
-//        mouse = new Vector3();
-//    }
+    protected GameStateManager gsm;
+
+    protected State(GameStateManager gsm) {
+        this.gsm = gsm;
+        cam = new OrthographicCamera();
+        mouse = new Vector3();
+    }
 
     protected abstract void handleInput();
     public abstract void update(float dt);
