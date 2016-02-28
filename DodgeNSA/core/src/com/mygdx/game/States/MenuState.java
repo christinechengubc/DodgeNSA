c5ca04991a64c9187b02ed86fcd708bbabdbf6d8
pacoleee <pacohotin@hotmail.com>
2016-02-27 19:43:58 -0800

    Part 5
    Paco: Changed menu state

--- DodgeNSA/core/src/com/mygdx/game/States/MenuState.java
+++ DodgeNSA/core/src/com/mygdx/game/States/MenuState.java
@@ -1,5 +1,6 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.DodgeNSA;
@@ -19,12 +20,15 @@ public class MenuState extends State {
 
     @Override
     public void handleInput() {
-
+        if(Gdx.input.justTouched()) {
+            gsm.set(new PlayState(gsm));
+            dispose();
+        }
     }
 
     @Override
     public void update(float dt) {
-
+        handleInput();
     }
 
     @Override
