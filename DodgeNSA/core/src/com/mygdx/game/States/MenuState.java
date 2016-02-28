7f8f9580009f65e601d08d95666523867ab8a05a
pacoleee <pacohotin@hotmail.com>
2016-02-27 19:29:59 -0800

    Part 4

--- DodgeNSA/core/src/com/mygdx/game/States/MenuState.java
+++ DodgeNSA/core/src/com/mygdx/game/States/MenuState.java
@@ -1,14 +1,20 @@
 package com.mygdx.game.States;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.DodgeNSA;
 
 /**
  * Created by pacolee on 16-02-27.
  */
 public class MenuState extends State {
+    private Texture background;
+    private Texture playBtn;
 
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("bg.jpg");
+        playBtn = new Texture("playBtn.jpg");
     }
 
     @Override
@@ -23,6 +29,10 @@ public class MenuState extends State {
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.begin();
+        sb.draw(background, 0, 0, DodgeNSA.WIDTH, DodgeNSA.HEIGHT);
+        sb.draw(playBtn, (DodgeNSA.WIDTH /2) - (playBtn.getWidth()/2), DodgeNSA.HEIGHT / 2);
+        sb.end();
 
     }
 }
